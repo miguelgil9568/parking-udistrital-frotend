bf0328ba0efbc774eef9307862b7081556adc4ef
Miguel Angel <72213940+miguelgil9568@users.noreply.github.com>
2023-03-21 11:19:30 -0500

    Configuracion inicial y creacion de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TableModule} from 'primeng/table';
+import {Car} from '../model/cars';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'parking-udistrital-frontend';
+  cars: Car[];
+  cols: any[];
+  username = '';
+
+  ngOnInit() {
+
+    this.cols = [
+      { field: 'vin', header: 'Vin' },
+      { field: 'year', header: 'Year' },
+      { field: 'brand', header: 'Brand' },
+      { field: 'color', header: 'Color' }
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,44 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {TableModule} from 'primeng/table';
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {DropdownModule} from "primeng/dropdown";
+import {ButtonModule} from "primeng/button";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {InputTextModule} from "primeng/inputtext";
+import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    DropdownModule,
+    ButtonModule,
+    InputTextModule,
+    InputTextareaModule,
+    TableModule,
+
+
+    HttpClientModule,
+    RouterModule,
+    ReactiveFormsModule,
   ],
   providers: [],
+  // exports: [
+  //   TableModule
+  // ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
